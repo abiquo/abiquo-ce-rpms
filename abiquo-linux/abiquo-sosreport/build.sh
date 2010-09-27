71ef9145db70167ab10283224a15a98415009e1c
Sergio Rubio <sergio@rubio.name>
2010-09-27 14:20:34 +0200

    fix sosreport-plugins build script

--- abiquo-linux/abiquo-sosreport/build.sh
+++ abiquo-linux/abiquo-sosreport/build.sh
@@ -1,24 +1,6 @@
-CHROOT=centos-5.4-x86_64
-CHROOT_DIR=/var/lib/mock/$CHROOT/root
-PKGNAME=abiquo-sosreport-plugins
+SIGN_PKGS=0
+MOCK_PROFILE=centos-5.5-x86_64_i386
+PKG_NAME=abiquo-sosreport-plugins
+BUILDBASE=~/rpmbuild
 
-mkdir tmp
-mkdir rpms
-
-rm abiquo*.py
-
-wget -q http://github.com/abiquo/abiquo-report-sos-plugin/raw/master/abiquo_kvm_node.py
-wget -q http://github.com/abiquo/abiquo-report-sos-plugin/raw/master/abiquo_xen_node.py
-wget -q http://github.com/abiquo/abiquo-report-sos-plugin/raw/master/abiquo_server.py
-
-/usr/bin/mock --init -r  $CHROOT
-
-/usr/bin/mock -r $CHROOT --copyin "abiquo_xen_node.py" /builddir/build/SOURCES/
-/usr/bin/mock -r $CHROOT --copyin "abiquo_kvm_node.py" /builddir/build/SOURCES/
-/usr/bin/mock -r $CHROOT --copyin "abiquo_server.py" /builddir/build/SOURCES/
-/usr/bin/mock -r $CHROOT --copyin $PKGNAME.spec /root
-/usr/bin/mock -r $CHROOT --shell "rpmbuild -ba /root/$PKGNAME.spec"
-
-cp $CHROOT_DIR/builddir/build/RPMS/*.rpm rpms
-cp $CHROOT_DIR/builddir/build/SRPMS/*.rpm rpms/SRPMS/
-rm -rf tmp
+source ~/Work/rpmdev/build.sh
